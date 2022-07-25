9ed7e7a8f19b0d0c0f1f9e1776cca7dfca3dc4e5
maiamea <maiameawork@gmail.com>
2022-07-25 10:48:24 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 'use strict';
-function fib(n) {
-  if (n === 0) {
-    return 0;
-  } else {
-    if (n === 1) {
-      return 1;
-    }
-  }
-  return fib(n-1) + fib(n-2);
-}
+// function fib(n) {
+//   if (n === 0) {
+//     return 0;
+//   } else {
+//     if (n === 1) {
+//       return 1;
+//     }
+//   }
+//   return fib(n-1) + fib(n-2);
+// }
 
-const number = process.argv[2] || 0;
-console.log(`${number}番目のフィボナッチ数は${fib(number)}です。`);
+// const number = process.argv[2] || 0;
+// console.log(`${number}番目のフィボナッチ数は${fib(number)}です。`);
+
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
+function fib(n) {
+  if (memo.has(n)) {
+    return memo.get(n);
+  }
+  const value = fib(n-1) + fib(n-2);
+  memo.set(n, value);
+  return value;
+}
 
 const length = 40;
 for (let i = 0; i <= length; i++) {
